31c51cc2e9c357c47687549c258300fc9e456799
Masafumi Okada <mokada@cuda.umin.ac.jp>
2017-08-17 00:26:00 +0900

    because this is not convolutional network (yet), change model names.

--- train.R
+++ train.R
@@ -4,9 +4,9 @@ load("Train15000.RData") # contains ohmasters, train_data
 X <- train_data$X
 Y <- train_data$Y
 
-model_cnn <- keras_model_sequential()
+model_nn <- keras_model_sequential()
 
-model_cnn %>% 
+model_nn %>% 
   layer_dense(units=1500, activation='relu', input_shape=dim(X)[2]) %>%
   layer_dropout(rate=0.1) %>% 
   layer_dense(units=500, activation = 'relu') %>%
@@ -14,10 +14,10 @@ model_cnn %>%
   layer_dense(units=dim(Y)[2], activation='sigmoid')
 
 #sgd <- optimizer_sgd(lr=0.01, decay=1e-6, momentum=0.9, nesterov=TRUE)
-model_cnn %>% compile(loss='binary_crossentropy',optimizer='adam',metrics='accuracy')
+model_nn %>% compile(loss='binary_crossentropy',optimizer='adam',metrics='accuracy')
 
-history_cnn <- model_cnn %>% fit(X, Y, epochs=100, batch_size=2000)
+history_nn <- model_nn %>% fit(X, Y, epochs=100, batch_size=2000)
 
 load("Eval10000.RData") # contains ohmasters, eval_data
-loss_and_metrics_cnn <- model_cnn %>% evaluate(eval_data$X, eval_data$Y)
+loss_and_metrics_nn <- model_nn %>% evaluate(eval_data$X, eval_data$Y)
 
