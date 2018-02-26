2b9467bcf0c2aca14632ed47217a961092e83834
Masafumi Okada <mf@okadajp.org>
2018-02-26 15:03:46 +0900

    - remove old functions(now using keras's features instead of original code)

--- DataUtils.R
+++ DataUtils.R
@@ -1,32 +1,17 @@
-txt2matrix <- function(text, master, useEmbedding=FALSE) {
-  contents <- sapply(text, function(x){strsplit(x,'')})[[1]]
-  if(useEmbedding) 
-    return(as.integer(factor(contents, levels=master)))
-  else
-    return(as.integer(master %in% contents))
-}
-
-eval_title <- function(model, title, categorymaster, titleschrmaster,maxtitlelength,useEmbedding=FALSE, threshould=0.5) {
-  n <- maxtitlelength - nchar(title)
-  et <- txt2matrix(paste(c(title, rep(' ',n)), collapse=''), titleschrmaster,useEmbedding)
-  et <- matrix(et, nrow=1)
-  if(!is.na(threshould))
-    categorymaster[which(model %>% predict(et) > threshould)]
-  else {
-    r <- as.vector(model %>% predict(et))
-    names(r) <- categorymaster
-    data.frame(score=sort(r,decreasing=TRUE))
-  }
-}
-
-eval_multilabel <- function(model, eval_data, threshould=0.5, method=c("identical", "xor"), allowance=0.1) {
+eval_multilabel <- function(model, eval_data, threshould=0.5, rankmax=5, method=c("identical", "rank", "xor", "maxidentical"), allowance=0.1) {
   method <- match.arg(method)
-  prediction <- 1*(predict(model, eval_data$X) > threshould)
-  eval <- sapply(1:dim(prediction)[1], function(i) {
-    if(method=="identical")
-      identical(as.integer(prediction[i,]), eval_data$Y[i,])
-    else if(method=="xor") # return TRUE only when proportion of disagreement between prediction and eval data is lower than allowance.
-      sum(xor(as.integer(prediction[i,]),eval_data$Y[i,]))/length(eval_data$Y[i,]) < allowance
-  })
+  prediction <- predict(model, eval_data$X)
+  eval <- sapply(1:dim(prediction)[1], 
+                 function(i) {
+                   if(method=="identical")
+                     identical(1.0*(prediction[i,] > threshould),eval_data$Y[i,])
+                   else if(method=="xor") # return TRUE only when proportion of disagreement between prediction and eval data is lower than allowance.
+                     sum(xor(1.0*(prediction[i,] > threshould),eval_data$Y[i,]))/length(eval_data$Y[i,]) < allowance
+                   else if(method=="rank") # return TRUE only when top (rankmax)th of predicted probability includes eval data.
+                     all(which(eval_data$Y[i,]==1) %in% (order(prediction[i,],decreasing=TRUE)[1:rankmax])) # all->anyとすると、categorical_accuracyと同値
+                   else if(method=="maxidentical")
+                     identical(which.max(prediction[i,]),which.max(eval_data$Y[i,]))
+                 })
   sum(eval) / length(eval)
 }
+
