dc6015061a16993bcf869d445e03683f0c7368bb
Masafumi Okada <mf@okadajp.org>
2018-03-01 13:24:04 +0900

    - move actual network structure to doTrain.R
    This file generates all combination of parameters to doTrain function, then call it, and collect accuracy information, then store the results into CSV files (build.csv, build-large.csv).

--- train.R
+++ train.R
@@ -1,26 +1,61 @@
 library(keras)
+source("DataUtils.R")
+source("doTrain.R")
+#use_backend("plaidml")
 
-load("Train15000.RData") # contains ohmasters, train_data
-X <- train_data$X
-Y <- train_data$Y
+set.seed(38934)
 
-model_nn <- keras_model_sequential()
+source=c("title", "purpose")
+tokenize=c("char", "word")
+embed=c("ohm", "seq")
+target=c("class", "type")
+network=c("dense", "rnn", "1dc")
 
-model_nn %>% 
-  layer_dense(units=1500, activation='relu', input_shape=dim(X)[2]) %>%
-  layer_dropout(rate=0.1) %>% 
-  layer_dense(units=500, activation = 'relu') %>%
-  layer_dropout(rate=0.1) %>%
-  layer_dense(units=dim(Y)[2], activation='sigmoid')
+build1 <- expand.grid(source,tokenize,embed,target,network, stringsAsFactors = FALSE)
+embed="bow"
+network="dense"
+build2 <- expand.grid(source,tokenize,embed,target,network, stringsAsFactors = FALSE)
+build <- rbind(build1, build2)
 
-#sgd <- optimizer_sgd(lr=0.01, decay=1e-6, momentum=0.9, nesterov=TRUE)
-model_nn %>% compile(loss='binary_crossentropy',optimizer='adam',metrics='accuracy')
+build$loss <- NA
+build$acc <- NA
+build$identical <- NA
+build$rank <- NA
+for(i in 1:nrow(build)) {
+  res <- doTrain(build[i,1], build[i,2], build[i,3], build[i,4], build[i,5], large=FALSE)
+  build$loss[i] <- res$loss_and_metrics$loss
+  build$acc[i] <- res$loss_and_metrics$categorical_accuracy
+  build$identical[i] <- res$eval_multilabel_identical
+  build$rank[i] <- res$eval_multilabel_rank
+}
 
-history_nn <- model_nn %>% fit(X, Y, epochs=100, batch_size=2000)
+write.csv(build, file="build.csv")
 
-load("Eval10000.RData") # contains ohmasters, eval_data
-loss_and_metrics_nn <- model_nn %>% evaluate(eval_data$X, eval_data$Y)
-eval_multilabel_nn <- eval_multilabel(model_nn, eval_data)
-model_nn_serialized <- serialize_model(model_nn)
-save(model_nn_serialized, file="model_nn.RData")
-save_model_hdf5(model_nn,"model_nn.hdf5") 
+#stop()
+source=c("title", "purpose")
+tokenize=c("char", "word")
+embed="seq"
+target=c("class", "type")
+network=c("dense", "rnn", "1dc")
+
+build1 <- expand.grid(source,tokenize,embed,target,network, stringsAsFactors = FALSE)
+embed="bow"
+network="dense"
+build2 <- expand.grid(source,tokenize,embed,target,network, stringsAsFactors = FALSE)
+build <- rbind(build1, build2)
+
+build$loss <- NA
+build$acc <- NA
+build$identical <- NA
+build$rank <- NA
+for(i in 1:nrow(build)) {
+  res <- doTrain(build[i,1], build[i,2], build[i,3], build[i,4], build[i,5], large=TRUE)
+  build$loss[i] <- res$loss_and_metrics$loss
+  build$acc[i] <- res$loss_and_metrics$categorical_accuracy
+  build$identical[i] <- res$eval_multilabel_identical
+  build$rank[i] <- res$eval_multilabel_rank
+}
+
+write.csv(build, file="build-large.csv")
+
+
