fab3a73674c265acfe38c142600f2f0158a6c441
Masafumi Okada <mf@okadajp.org>
2018-03-01 14:33:00 +0900

    - updated to use models of new version.

--- server.R
+++ server.R
@@ -5,19 +5,70 @@
 # http://shiny.rstudio.com
 #
 
-library(keras)
 library(shiny)
+library(keras)
+library(RMeCab)
+library(reticulate)
+
 options(shiny.sanitize.errors = TRUE)
 
-load("model_nn.RData")
-load("Eval10000.RData")
 source("DataUtils.R")
-model_nn <- unserialize_model(model_nn_serialized)
-
+load("data_large/PredictionMasters.RData")
 
 shinyServer(function(input, output) {
+  modelspec <- reactive({
+    validate(
+      need( input$embed != "bow" || (input$embed == "bow" && input$network == "dense"), "Bag of Wordsの時はRNNと畳み込みは利用できません")
+    )
+    m1 <- paste("model_large/m_", switch(input$embed,
+            "bow"="bow",
+            "seq"="seq"), "_purpose_",
+          switch(input$kakasi,
+            "char"="char",
+            "word"="word"), "_", sep="")
+    m2 <- paste("_", switch(input$network,
+            "dense"="dense",
+            "rnn"="rnn",
+            "1dc"="1dc"),".hdf5",sep="")
+    list(class=paste(m1,"class", m2,sep=""),
+         type=paste(m1,"type",m2,sep=""))
+  })
+  tokenizerspec <- reactive({
+    paste("data_large/purpose_",
+          switch(input$kakasi, 
+                 "char"="char",
+                 "word"="word"), "_tokenizer.keras", sep="")
+  })
+
+  isword <- reactive({
+    switch(input$kakasi,
+           "char"=FALSE,
+           "word"=TRUE)    
+  })
+  isbow <- reactive({
+    switch(input$embed,
+           "bow"=TRUE,
+           FALSE)    
+  })
+  
   output$class <- renderText({
-    eval_title(model_nn, input$text, ohmasters$categoryMaster, ohmasters$titlesCharactersMaster, ohmasters$max_title_length)
+    input$eval
+    model <- isolate(load_model_hdf5(modelspec()$class))
+    tokenizer <- isolate(load_text_tokenizer(tokenizerspec()))
+    if(isolate(input$text)=="")
+      "判定対象なし"
+    else
+      isolate(eval_text(input$text, model, tokenizer, master=DiseaseClassesMaster, word=isword(), bow=isbow()))
+  })    
+  output$type <- renderText({
+    input$eval
+    model <- isolate(load_model_hdf5(modelspec()$type))
+    tokenizer <- isolate(load_text_tokenizer(tokenizerspec()))
+    if(isolate(input$text)=="")
+      "判定対象なし"
+    else
+      isolate(eval_text(input$text, model, tokenizer, master=TypesMaster, word=isword(), bow=isbow()))
   })
 
 })
+
--- ui.R
+++ ui.R
@@ -11,10 +11,14 @@ shinyUI(fluidPage(
 
   # Application title
   titlePanel("Demonstration of Text Classification"),
-  p("UMIN-CTRのデータを学習させたニューラルネットワークを用いて、文章から関連する医学分野を推定します。"),
+  p("UMIN-CTRのデータを学習させたニューラルネットワークを用いて、文章から関連する医学分野および研究の種類を推定します。"),
   a(href="https://github.com/mokjpn/TargetDiseaseClassifier/", "詳細はこちら"),
   textAreaInput("text",  "研究目的を入力してください"),
-  submitButton("推定"),
-  p("推定される分野："), textOutput("class")
-    )
+  selectInput("kakasi", "利用するモデルの単語分割方式を選択してください", choices=c("文字単位"="char", "単語単位"="word"), selected="word"),
+  selectInput("embed", "利用するモデルの文字埋め込み方式を選択してください", choices=c("Bag of Words"="bow", "Embedding Layer"="seq"), selected="bow"),
+  selectInput("network", "利用するモデルの構造を選択してください", choices=c("全結合"="dense", "RNN"="rnn", "1次元畳み込み"="1dc"), selected="dense"),
+  actionButton("eval", "推定"),
+  p("推定される分野："), textOutput("class"),
+  p("推定される種別："), textOutput("type")
+)
 )
